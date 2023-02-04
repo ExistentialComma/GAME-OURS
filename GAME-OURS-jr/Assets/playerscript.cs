35aac5de448b0e3173b8f946f24f63f36e610591
ExistentialComma <noexistentialsin@gmail.com>
2023-02-05 00:30:10 +0200

    adding new movement

---
+++ GAME-OURS-jr/Assets/playerscript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+public class playerscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float pozX;
+    public float pozY;
+    public Transform ori;
+    float xrot;
+    float yrot;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void FixedUpdate()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * pozX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * pozY;
+        xrot += mouseX;
+        yrot -= mouseY;
+        xrot = Mathf.Clamp(xrot, -90f, 90f);
+        transform.rotation = Quaternion.Euler(xrot, yrot, 0);
+        ori.rotation = Quaternion.Euler(0, yrot, 0);
+    }
+}
+
